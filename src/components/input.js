491df75e8d1b4da01b244835d85abba10579599c
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-19 17:38:13 -0600

    added some styles

---
+++ src/components/input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Input = (title) => {
+
+    return (
+        <div className="input">
+            <input />
+        </div>
+    )
+  }
+
+
+export default Input; 
