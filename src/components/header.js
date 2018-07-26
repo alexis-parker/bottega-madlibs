9b33b8e1aec78a3bd3b4a8ead857580434e999b3
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-26 10:31:01 -0600

    created background header skrews

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className="header">
+    <div className="header">
+        <div className="header_skew"></div>
+        <div className="header_border"></div>
              <h1>Bottega Madlibs</h1>
              <p>Fill out the fields below and click and click the Generate button to see the Mad Lib story.</p>
         </div>
